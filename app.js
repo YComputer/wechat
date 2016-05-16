97aea88349c89da739200b20da8cb74ac5d886be
YComputer <thea@163.com>
2016-05-16 23:00:20 +0800

    change listen port to 80

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict'
 
 var Koa = require('koa')
-var sha1 = require('sha1')
+var wechat = require('./wechat/g')
 var config = {
     wechat: {
         appID: 'wxb6947ead7fe5b0df',
@@ -12,24 +12,8 @@ var config = {
 
 var app = new Koa()
 
-app.use(function*(next) {
-    console.log(this.query)
-
-    var token = config.wechat.token
-    var signature = this.query.signature
-    var nonce = this.query.nonce
-    var timestamp = this.query.timestamp
-    var echostr = this.query.echostr
-    var str = [token, timestamp, nonce].sort().join('')
-    var sha = sha1(str)
-    if (sha === signature) {
-        this.body = echostr + ''
-    } else {
-        this.body = 'wrong'
-    }
-
-})
+app.use(wechat(config.wechat))
 
 app.listen(80)
 
-console.log('listening: 1234')
+console.log('listening: 80')
