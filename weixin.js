0ad05b0816ae6861f4018e114ac69b62cf2795f0
YComputer <thea@163.com>
2016-05-18 00:18:48 +0800

    custom replay message

--- weixin.js
+++ weixin.js
@@ -14,11 +14,32 @@ exports.reply = function* (next){
 			//可以记录到数据库中查看取消订阅的用户信息
 			console.log('取消关注')
 			this.body = ''
+		}else if(message.Event === 'LOCATION'){
+			this.body = '你上报的位置是：' + message.Latitude + '/' + message.Longitude + '-' + message.Precision
+		}else if(message.Event === 'CLICK'){
+			this.body = '你点击了菜单：' + message.EventKey
+		}else if(message.Event === 'SCAN'){
+			console.log('关注后扫描二维码'+message.EventKey+' '+ message.Ticket)
+
+			this.body = '确认你已经扫描了二维码'
+		}else if(message.Event === 'VIEW'){
+			this.body = '你点击了菜单中的连接：'+ message.EventKey
 		}
 
 
-	}else{
+	}else if(message.MsgType === 'text'){
+		var content = message.Content
+		var reply = '你说的 '+ message.Content + ' 太复杂了'
 
+		if(content === 1){
+			reply = '你输入了1'
+		}else if(content === 2){
+			reply = '你输入了2'
+		}else if(content === 3){
+			reply = '你输入了3'
+		}
+
+		this.body = reply
 	}
 
 	yield next
