8a5bb997a99ed217cc537cde896305e99888ca12
YComputer <thea@163.com>
2016-05-17 11:40:18 +0800

    modularization

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 var koa = require('koa')
 var path = require('path')
 var wechat = require('./wechat/g')
-var util require('./libs/util')
+var util = require('./libs/util')
 var wechat_file = path.join(__dirname, './config/wechat.txt')
 
 var config = {
