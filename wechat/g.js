8e3d2a9b69abc33e053c00cc2eb753b9c571a973
YComputer <thea@163.com>
2016-05-17 23:34:55 +0800

    custom replay message

--- wechat/g.js
+++ wechat/g.js
@@ -9,9 +9,9 @@ var util = require('./util')
 module.exports = function(opts) {
 
     // 这里注释掉和不注释掉的区别在哪里？
-    //var wechat = new Wechat(opts)
+    var wechat = new Wechat(opts)
 
-    return function*(next) {
+    return function* (next) {
         console.log('query from weixin--->',this.query)
         var that = this
         var token = opts.token
