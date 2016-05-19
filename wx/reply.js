95b80a56d0e005510bbc783b9751dd48b5c7bbd0
YComputer <thea@163.com>
2016-05-20 00:00:21 +0800

    debug

--- wx/reply.js
+++ wx/reply.js
@@ -6,9 +6,10 @@ var Wechat = require('../wechat/wechat')
 var menu = require('./menu')
 var wechatApi = new Wechat(config.wechat)
 
-// wechatApi.deleteMenu().then(function(data){
-//     return wechatApi.createMenu(menu)
-// }).then(function(msg){
+wechatApi.deleteMenu().then(function(data){
+    //return wechatApi.createMenu(menu)
+})
+// .then(function(msg){
 //     console.log('after createMenu-->',msg)
 // })
 
